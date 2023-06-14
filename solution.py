5861ab9f485f86352c2798e55f88909c8237618e
Kathryn Deeks <119135727+r3allytalltruth@users.noreply.github.com>
2023-06-13 17:03:42 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,85 +1,66 @@
-from math import sqrt
-from numpy import zeros
+from math import gcd
 
 
-def computeDist(p1, p2):
-    return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-
-
-def computeGCD(x, y):
-    while(y):
-        x, y = y, x % y
-    return abs(x)
-
-
-def get_entity_position_from_room_number(entity, room_number, dimensions):
-    r_x, r_y = room_number
-    e_x, e_y = entity
+def get_entity_position(entity, room_number, dimensions):
+    room_x, room_y = room_number
+    entity_x, entity_y = entity
     dim_x, dim_y = dimensions
 
-    res_x = dim_x*r_x + e_x if r_x % 2 == 0 else dim_x*r_x + (dim_x - e_x)
-    res_y = dim_y*r_y + e_y if r_y % 2 == 0 else dim_y*r_y + (dim_y - e_y)
+    res_x = dim_x * room_x + entity_x if room_x % 2 == 0 else dim_x * room_x + (dim_x - entity_x)
+    res_y = dim_y * room_y + entity_y if room_y % 2 == 0 else dim_y * room_y + (dim_y - entity_y)
 
     return (res_x, res_y)
 
 
 def solution(dimensions, your_position, trainer_position, distance):
-
     dim_x, dim_y = dimensions
-    m_x, m_y = your_position
+    your_x, your_y = your_position
+    trainer_x, trainer_y = trainer_position
 
-    num_rooms_above_x_axis = (distance + m_y)//dim_y + 1
-    num_rooms_below_x_axis = (distance - m_y)//dim_y + 1
-    num_rooms_left_of_y_axis = (distance - m_x)//dim_x + 1
-    num_rooms_right_of_y_axis = (distance + m_x)//dim_x + 1
+    width = (2 * distance // dim_x + 1) * dim_x + 1
+    height = (2 * distance // dim_y + 1) * dim_y + 1
 
-    w = (num_rooms_right_of_y_axis + num_rooms_left_of_y_axis)*dim_x + 1
-    h = (num_rooms_above_x_axis + num_rooms_below_x_axis)*dim_y + 1
-
-    x_offset = num_rooms_left_of_y_axis*dim_x
-    y_offset = num_rooms_below_x_axis*dim_y
-
-    matrix = zeros(shape=(w, h))
-    for i in range(-1*num_rooms_left_of_y_axis, num_rooms_right_of_y_axis):
-        for j in range(-1*num_rooms_below_x_axis, num_rooms_above_x_axis):
-            tv_x, tv_y = get_entity_position_from_room_number(
-                trainer_position, [i, j], dimensions)
-
-            mv_x, mv_y = get_entity_position_from_room_number(
-                your_position, [i, j], dimensions)
-
-            matrix[tv_x+x_offset][tv_y+y_offset] = 1
-            matrix[mv_x+x_offset][mv_y+y_offset] = 2
+    matrix = [[0] * height for _ in range(width)]
 
     hits = 0
     shots_taken = set()
-    for i in range(-1*num_rooms_left_of_y_axis, num_rooms_right_of_y_axis):
-        for j in range(-1*num_rooms_below_x_axis, num_rooms_above_x_axis):
-            t_x, t_y = get_entity_position_from_room_number(
-                trainer_position, [i, j], dimensions)
-            if distance < computeDist([t_x, t_y], your_position):
+
+    for room_x in range(-distance // dim_x - 1, distance // dim_x + 2):
+        for room_y in range(-distance // dim_y - 1, distance // dim_y + 2):
+            trainer_pos = get_entity_position(trainer_position, (room_x, room_y), dimensions)
+            player_pos = get_entity_position(your_position, (room_x, room_y), dimensions)
+
+            matrix[trainer_pos[0]][trainer_pos[1]] = 1
+            matrix[player_pos[0]][player_pos[1]] = 2
+
+    for room_x in range(-distance // dim_x - 1, distance // dim_x + 2):
+        for room_y in range(-distance // dim_y - 1, distance // dim_y + 2):
+            trainer_pos = get_entity_position(trainer_position, (room_x, room_y), dimensions)
+
+            if distance < ((trainer_pos[0] - your_x) ** 2 + (trainer_pos[1] - your_y) ** 2) ** 0.5:
                 continue
-            delta_y = t_y - m_y
-            delta_x = t_x - m_x
-            d = computeGCD(delta_y, delta_x)
-            delta_y = int(delta_y/d)
-            delta_x = int(delta_x/d)
-            if (delta_y, delta_x) in shots_taken:
+
+            delta_x = trainer_pos[0] - your_x
+            delta_y = trainer_pos[1] - your_y
+            gcd_val = gcd(delta_x, delta_y)
+            delta_x //= gcd_val
+            delta_y //= gcd_val
+
+            if (delta_x, delta_y) in shots_taken:
                 continue
-            shots_taken.add((delta_y, delta_x))
-            ray_x, ray_y = m_x + x_offset, m_y + y_offset
+
+            shots_taken.add((delta_x, delta_y))
+
+            pos_x, pos_y = your_x, your_y
+
             while True:
-                ray_x += delta_x
-                ray_y += delta_y
-                entity = matrix[ray_x][ray_y]
-                if entity == 1:
+                pos_x += delta_x
+                pos_y += delta_y
+
+                if matrix[pos_x][pos_y] == 1:
                     hits += 1
                     break
-                elif entity == 2:
+                elif matrix[pos_x][pos_y] == 2:
                     break
+
     return hits
-
-
-if __name__ == "__main__":
-    print(solution([3, 2], [1, 1], [2, 1], 4))
-    print(solution([300, 275], [150, 150], [185, 100], 500))
